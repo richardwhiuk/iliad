5f0f79bce5bae2f3a091a4bbb033782d30002bdd
Richard Whitehouse <github@richardwhiuk.com>
2011-02-13 19:07:34 +0000

    Clear up loaders

--- src/server.cpp
+++ src/server.cpp
@@ -6,6 +6,10 @@ Iliad::Server::Server(){
 
 Iliad::Server::~Server(){
 
+	for(std::map<std::string, Loader*>::iterator it = loaders.begin(); it != loaders.end(); it ++){
+		delete (it->second);
+	}
+
 }
 
 void Iliad::Server::process(Iliad::Request& req, Iliad::Response& rep){
