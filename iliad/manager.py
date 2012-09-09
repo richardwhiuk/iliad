b97b3aab8a3c01d77c978588fb2b6f19a26a3040
Richard Whitehouse <github@richardwhiuk.com>
2012-09-09 12:17:37 +0100

    Run in a forked process, waiting for completion

--- iliad/manager.py
+++ iliad/manager.py
@@ -1,6 +1,18 @@
 import preforkserver
+import os
 
 class Manager(preforkserver.Manager):
 	def preBind(self):
 		pass
+
+	def run(self):
+		pid = os.fork()
+		if pid:
+			self.__pid = pid
+		else:
+			preforkserver.Manager.run(self)
+			os._exit(0)
+
+	def wait(self):
+		os.waitpid(self.__pid, 0)
 
