69741da00e188d44958f1a0200c9f276918d465b
Richard Whitehouse <github@richardwhiuk.com>
2012-09-10 11:43:51 +0100

    Add the ability to edit content pages

--- iliad/content.py
+++ iliad/content.py
@@ -1,4 +1,5 @@
 import iliad.core.module
+import iliad.form
 
 class Module(iliad.core.module.Module):
 	
@@ -52,6 +53,8 @@ class Page:
 	class Main:
 
 		def __init__(self, resource, prefix, data):
+			self._submit = None
+			self._redirect = None
 			self._resource = resource
 			arg = resource.get_argument()
 			if arg == 'new':
@@ -67,25 +70,71 @@ class Page:
 				except ValueError:
 					self.list()
 					return
-				
+
 				arg = resource.get_argument()
+
 				if arg == 'view':
 					resource.pop_argument()
 					self.view()
 				elif arg == 'edit':
 					resource.pop_argument()
-					self.edit()
+					self.edit(prefix + '/' + str(self._content.id()) + '/edit', data(str(self._content.id()))('edit'))
 				elif arg == 'delete':
 					resource.pop_argument()
 					self.delete()
 				else:
 					self.view()
 
+		def save(self):
+			self._submit = 'Save'
+
+		def save_continue(self):
+			self._submit = 'Save Continue'
+
+		def preview(self):
+			self._submit = 'Preview'
+			self._preview = True
+
 		def view(self):
 			self._mode = 'view'
+
+		def edit(self, xiPrefix, xiData):
+			self._mode = 'edit'
+			self._preview = False
+
+			self._form = iliad.form.Form(
+				prefix=xiPrefix,
+				action='',
+				fields=[
+					{ 'name': 'title', 'label': 'Title', 'type': 'iliad.form.text', 'value': self._content.title },
+					{ 'name': 'body', 'label': 'Body', 'type': 'iliad.form.textarea', 'value': self._content.body },
+					{ 'name': 'preview', 'label': 'Preview', 'type': 'iliad.form.submit', 'value': self.preview },
+					{ 'name': 'save-continue', 'label': 'Save and Continue', 'type': 'iliad.form.submit', 'value': self.save_continue },
+					{ 'name': 'save', 'label': 'Save', 'type': 'iliad.form.submit', 'value': self.save }
+				],
+				data=xiData
+			)
+
+			if self._submit:
+				if self._submit == 'Save' or self._submit == 'Save Continue':
+					self._content.save()
+				if self._submit == 'Save':
+					self._redirect = 'view'
+				elif self._submit == 'Save Continue':
+					self._redirect = 'edit'
 
 		def render(self, env):
 			if self._mode == 'view':
 				template = env.get_template('page/main/content/view.html')
 				return (False, template.render(body=self._content.html()))
+			elif self._mode == 'edit':
+				if self._redirect:
+					return (True, self._redirect)
+				else:
+					template = env.get_template('page/main/content/edit.html')
+					form = self._form.render(env)
+					if form[0]:
+						return form
+					return (False, template.render(form=form[1], preview=self._preview, body=self._content.html()))
+
 
