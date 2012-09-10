8d259c364dc31f7e2bed1ae5e69ceb351cf1d9ea
Richard Whitehouse <github@richardwhiuk.com>
2012-09-10 17:14:02 +0100

    Encapsulate any argument passed with the resource

--- iliad/core/resource.py
+++ iliad/core/resource.py
@@ -10,8 +10,9 @@ class Resource:
 		self._output = output
 		self._logic = logic
 		self._base = base
-		self.additional = additional
+		self._additional = additional
 		self._arguments = None
+		self._argument = None
 
 	def id(self):
 		return self._id
@@ -21,11 +22,21 @@ class Resource:
 			self._arguments = []
 			if len(self._base) > 0:
 				self._arguments += self._base.strip('/').split('/')
-			if len(self.additional) > 0:
-				self._arguments += self.additional.strip('/').split('/')
+			if self._argument:
+				self._arguments += self._argument.strip('/').split('/')
+			elif len(self._additional) > 0:
+				self._arguments += self._additional.strip('/').split('/')
 		if len(self._arguments) > 0:
 			return self._arguments[0]
 		return None
+
+	def argument(self, argument):
+		if self._arguments:
+			raise Exception("Arguments already defined.")
+		else:
+			argument = argument.strip('/')
+			if argument != self._additional and len(argument) > 0:
+				self._argument = argument
 
 	def pop_argument(self):
 		self._arguments.pop(0)
@@ -49,8 +60,8 @@ def Get(paths=None, path=None, argument=None):
 
 		if len(argument) > 0:
 			for resource in resources:
-				resource.additional = argument
-		
+				resource.argument(argument)
+
 		return resources
 		
 	elif path != None:
